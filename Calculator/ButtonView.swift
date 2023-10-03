692d1c446eb867c6cf8814943dcd6f77654b449c
Sebastian Wiktor <sebastian.wiktor@pollub.edu.pl>
2023-10-03 17:48:47 +0200

    finished view layout

--- Calculator/ButtonView.swift
+++ Calculator/ButtonView.swift
@@ -8,11 +8,14 @@
 import SwiftUI
 
 struct ButtonView: View {
+    @State var text: String = ""
+    @State var width: CGFloat = 0
+    @State var height: CGFloat = 0
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(text).foregroundStyle(.blue).padding().frame(width: width, height: height).border(Color.black, width: 2.0).font(.largeTitle)
     }
 }
 
-#Preview {
-    ButtonView()
-}
+//#Preview {
+//    ButtonView()
+//}
--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -10,12 +10,37 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("KALKULATOR").font(.largeTitle)
+            Spacer().frame(height: 100)
+            Text("").padding().frame(width: 315.0, height: 60.0).border(Color.black, width: 2.0)
+            HStack {
+                ButtonView(text: "1", width: 100.0, height: 60.0)
+                ButtonView(text: "2", width: 100.0, height: 60.0)
+                ButtonView(text: "3", width: 100.0, height: 60.0)
+            }
+            HStack {
+                ButtonView(text: "4", width: 100.0, height: 60.0)
+                ButtonView(text: "5", width: 100.0, height: 60.0)
+                ButtonView(text: "6", width: 100.0, height: 60.0)
+            }
+            HStack {
+                ButtonView(text: "7", width: 100.0, height: 60.0)
+                ButtonView(text: "8", width: 100.0, height: 60.0)
+                ButtonView(text: "9", width: 100.0, height: 60.0)
+            }
+            HStack {
+                ButtonView(text: "0", width: 100.0, height: 60.0)
+                ButtonView(text: "+", width: 100.0, height: 60.0)
+                ButtonView(text: "-", width: 100.0, height: 60.0)
+            }
+            HStack {
+                ButtonView(text: "*", width: 100.0, height: 60.0)
+                ButtonView(text: "/", width: 100.0, height: 60.0)
+                ButtonView(text: "sin", width: 100.0, height: 60.0)
+            }
+            ButtonView(text: "Oblicz", width: 315.0, height: 60.0)
+            
         }
-        .padding()
     }
 }
 
